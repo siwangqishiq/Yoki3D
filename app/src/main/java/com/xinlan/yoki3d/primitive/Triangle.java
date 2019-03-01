a8ebcfae589d3bd8fd84b9c7653c67040baeb8df
panyi <yuyi.py@alibaba-inc.com>
2019-03-01 19:41:25 +0800

    add triangle primitive

--- app/src/main/java/com/xinlan/yoki3d/primitive/Triangle.java
+++ app/src/main/java/com/xinlan/yoki3d/primitive/Triangle.java
@@ -1,5 +1,7 @@
 package com.xinlan.yoki3d.primitive;
 
+import android.opengl.GLES30;
+
 import com.xinlan.yoki3d.R;
 import com.xinlan.yoki3d.utils.OpenglEsUtils;
 import com.xinlan.yoki3d.utils.ShaderUtil;
@@ -8,30 +10,43 @@ import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 public class Triangle extends RenderNode {
-    protected float[] mPoints = new float[2 * 3];
+    protected float[] mPoints = new float[3 * 3];
     protected FloatBuffer mPointBuf;
 
     protected float[] mColor = new float[4];
-    protected ByteBuffer mColorBuf;
-
-    protected float mLineWidth;
 
     public Triangle() {
         mPointBuf = OpenglEsUtils.allocateBuf(mPoints);
-        mProgramId = ShaderUtil.buildShaderProgram(R.raw.line_vertex, R.raw.line_frg);
+        mProgramId = ShaderUtil.buildShaderProgram(R.raw.triangle_vertex, R.raw.triangle_frg);
     }
 
-    public void setLine(float x1, float y1, float z1,
-                        float x2, float y2, float z2) {
+    public void setTriangle(float x1, float y1, float z1,
+                            float x2, float y2, float z2,
+                            float x3, float y3, float z3) {
+        mPointBuf.position(0);
+        mPointBuf.put(x1);
+        mPointBuf.put(y1);
+        mPointBuf.put(z1);
 
+        mPointBuf.put(x2);
+        mPointBuf.put(y2);
+        mPointBuf.put(z2);
+
+        mPointBuf.put(x3);
+        mPointBuf.put(y3);
+        mPointBuf.put(z3);
+
+        mPointBuf.position(0);
     }
 
     public void setColor(int r, int g, int b, int a) {
-
+        OpenglEsUtils.convertColor(r, g, b, a, mColor);
     }
 
     @Override
     public void render() {
+        GLES30.glUseProgram(mProgramId);
 
     }
+
 }//end class
